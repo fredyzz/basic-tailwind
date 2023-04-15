c08acd4ef5b326a27b5c833e1192218d637991c9
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2023-04-15 15:44:50 +0200

    Extract btn-primary styles

--- src/components/card/components/actions/components/buttons/learnMore/index.js
+++ src/components/card/components/actions/components/buttons/learnMore/index.js
@@ -1,9 +1,6 @@
 const ButtonLearnMore = ({ buttonLink, buttonText }) => (
   <div>
-    <a
-      href={buttonLink}
-      className="bg-blue-500 text-white px-4 py-2 mt-4 rounded hover:bg-blue-600 transition-colors duration-300"
-    >
+    <a href={buttonLink} className="btn-primary">
       {buttonText}
     </a>
   </div>
